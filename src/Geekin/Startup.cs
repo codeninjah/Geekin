8fbff755e6a8149a40f1b7f280f9593c01cf683c
codeninjah <codeninjahblog@gmail.com>
2016-12-01 11:07:13 +0100

    Har gjort en random kommentar.

--- src/Geekin/Startup.cs
+++ src/Geekin/Startup.cs
@@ -20,6 +20,7 @@ namespace Geekin
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        //Random comment
         public void Configure(IApplicationBuilder app)
         {
             app.UseIISPlatformHandler();
